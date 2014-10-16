b4a6351e2ed55d9a646d11d0099b5eae0568c94e
Nathan Villaescusa <nathan@chartio.com>
2014-10-16 15:11:03 -0700

    Push settings out, add views.

---
+++ torment/settings.py
@@ -0,0 +1,13 @@
+TWILIO_ACCOUNT_SID = "AC2a736300bab7af80b994e47ba06761cc"
+TWILIO_AUTH_TOKEN = "759725fdf14a724773c91142f53d54e0"
+TWILIO_FROM_NUMBER = "+17345473717"
+
+IMAGES = [
+    'http://cdn1.tnwcdn.com/wp-content/blogs.dir/1/files/2013/12/Hot-Dog-3.jpg'
+    'http://cdn1.tnwcdn.com/wp-content/blogs.dir/1/files/2013/12/miley-cyrus-vma-twerk.gif'
+]
+
+PEOPLE = {
+    'nathan': '+19192000373',
+    'nathanz': '+1919-904-9853'
+}
--- torment/views.py
+++ torment/views.py
@@ -1,6 +1,38 @@
 from torment.web import app
 
+from torment import torment
+from torment import settings
+
+
+def who():
+    return settings.PEOPLE['nathan']
+
 
 @app.route('/')
 def index():
     return 'this was a test'
+
+
+@app.route('/hotdog')
+def hotdog():
+    torment.hotdog(who())
+
+
+@app.route('/yell')
+def yell():
+    torment.call(who())
+
+
+@app.route('/text')
+def text():
+    torment.text(who())
+
+
+@app.route('/image')
+def image():
+    torment.image(who())
+
+
+@app.route('/surprise')
+def surprise():
+    torment.random_image(who())

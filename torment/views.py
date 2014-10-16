68561fcf41a17a133fc22475f0a2644588063db9
Nathan Villaescusa <nathan@chartio.com>
2014-10-16 15:40:05 -0700

    404 Support

--- torment/views.py
+++ torment/views.py
@@ -1,10 +1,15 @@
-from torment.web import app
+from flask import abort
+
 
 from torment import api
+from torment.web import app
 
 
-def who(person):
-    return api.get_phone_number(person)
+def get_pn(person):
+    try:
+        return api.get_phone_number(person)
+    except KeyError:
+        abort(404)
 
 
 @app.route('/')
@@ -14,24 +19,24 @@ def index(person):
 
 @app.route('/<person>/hotdog')
 def hotdog(person):
-    api.hotdog(who(person))
+    api.hotdog(get_pn(person))
 
 
 @app.route('/<person>/yell')
 def yell(person):
-    api.call(who(person))
+    api.call(get_pn(person))
 
 
 @app.route('/<person>/text')
 def text(person):
-    api.text(who(person))
+    api.text(get_pn(person))
 
 
 @app.route('/<person>/image')
 def image(person):
-    api.image(who(person))
+    api.image(get_pn(person))
 
 
 @app.route('/<person>/surprise')
 def surprise(person):
-    api.random_image(who(person))
+    api.random_image(get_pn(person))
